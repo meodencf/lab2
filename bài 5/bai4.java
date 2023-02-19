86614b6567113ce12fc515e036a9dcda103e8f0b
meodencf <125757693+meodencf@users.noreply.github.com>
2023-02-19 22:07:48 +0700

    add

--- bài 5/bai4.java
+++ bài 5/bai4.java
@@ -1,33 +1,38 @@
 package baiap2;
 
+import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class bai4 {
-    public static void main(String[] args) {     
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("nhap so luong phan tu: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
         System.out.println("nhap phan tu: ");
-        int n = sc.nextInt(); 
-        int[] a = new int[n];
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt(); 
-        }
-        int x = sc.nextInt(); 
-        int[] b = new int[n]; 
-        int k = 0; 
+            arr[i] = scanner.nextInt();
+        } 
+        System.out.print("Nhap x de xoa: ");
+        int x = scanner.nextInt();
+        int index = -1;
         for (int i = 0; i < n; i++) {
-            if (a[i] != x) {
-                b[k] = a[i]; 
-                k++; 
+            if (arr[i] == x) {
+                index = i;
+                break;
             }
         }
-        if (k == 0) { 
-            System.out.println("Khong co phan tu nao khac " + x);
-        } else { // nếu có phần tử
-            System.out.print("Mang sau khi xoa phan tu " + x + " la: ");
-            for (int i = 0; i < k; i++) {
-                System.out.print(b[i] + " "); 
+        if (index != -1) {
+            for (int i = index; i < n - 1; i++) {
+                arr[i] = arr[i + 1];
             }
+            n--;
+        }    
+        Arrays.sort(arr);       
+        System.out.print("mang sau khi xoa" + x );
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
-        sc.close(); 
     }
 }
